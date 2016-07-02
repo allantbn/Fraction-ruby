27caaf5e28ea5b45baff728e04ccb848917c48b6
allantbn <allan.tan@titansoft.com.sg>
2016-07-03 02:20:12 +0800

    Removed if else in plus function

--- math/fraction.rb
+++ math/fraction.rb
@@ -5,11 +5,7 @@ class Fraction
 	end
 
 	def plus(another_integerValue)
-		if another_integerValue.integerValue > 0
 			Fraction.new(@integerValue + another_integerValue.integerValue)
-		else 
-			Fraction.new(@integerValue)
-		end
 	end
 	
 	def int_value
