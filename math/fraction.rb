a3ec6bb888dfabd7445caf4497c139fdda1f051a
allantbn <allan.tan@titansoft.com.sg>
2016-07-03 02:06:27 +0800

    add non zero to zero
    Added attr_reader and if else into plus function

--- math/fraction.rb
+++ math/fraction.rb
@@ -1,10 +1,15 @@
 class Fraction
+	attr_reader :integerValue
 	def initialize (integerValue)
 		@integerValue = integerValue
 	end
 
-	def plus(another)
-		Fraction.new(@integerValue)
+	def plus(another_integerValue)
+		if another_integerValue.integerValue > 0
+			Fraction.new(another_integerValue.integerValue)
+		else 
+			Fraction.new(@integerValue)
+		end
 	end
 	
 	def int_value
--- spec/fraction_spec.rb
+++ spec/fraction_spec.rb
@@ -1,13 +1,18 @@
 require "./math/fraction"
 
 describe "Add Fractions Test" do
-	it "adds 0 + 0" do
+	it "adds zero plus zero" do
 	sum = Fraction.new(0).plus(Fraction.new(0)).int_value
 	sum.should == 0
 	end 
 	
-	it "adds 3 + 0" do
+	it "adds nonzero plus zero" do
 	sum = Fraction.new(3).plus(Fraction.new(0)).int_value
 	sum.should == 3
 	end 
+	
+	it "adds zero plus nonzero" do
+	sum = Fraction.new(0).plus(Fraction.new(5)).int_value
+	sum.should == 5
+	end
 end
