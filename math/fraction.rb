51538ab6125a3710744d793b02d944dcd546e821
allantbn <allan.tan@titansoft.com.sg>
2016-07-03 15:39:46 +0800

    non trivial denominator

--- math/fraction.rb
+++ math/fraction.rb
@@ -1,16 +1,19 @@
 class Fraction
 	attr_reader :integerValue
-	def initialize (integerValue)
-		@integerValue = integerValue
+	attr_reader :denominatorValue
+	def initialize (numerator, denominator = 1)
+		@integerValue = numerator
+		@denominatorValue = denominator
 	end
 
 	def plus(another_integerValue)
-			Fraction.new(@integerValue + another_integerValue.integerValue)
+			Fraction.new(@integerValue + another_integerValue.integerValue, @denominatorValue)
 	end
 	
 	def int_value
 		@integerValue
 	end
+	
 end
 
 
--- spec/fraction_spec.rb
+++ spec/fraction_spec.rb
@@ -25,4 +25,10 @@ describe "Add Fractions Test" do
 	sum = Fraction.new(-3).plus(Fraction.new(1)).int_value
 	sum.should == -2
 	end
+	
+	it "non trivial denominator" do
+	sum = Fraction.new(1, 5).plus(Fraction.new(2, 5))
+	sum.integerValue.should == 3
+	sum.denominatorValue.should == 5
+	end
 end
