c696994073db0cf586173ff5e02b0c2a32ed1227
allantbn <allan.tan@titansoft.com.sg>
2016-07-07 17:53:04 +0800

    Added reflective test

--- spec/greatestCommonDivisor_spec.rb
+++ spec/greatestCommonDivisor_spec.rb
@@ -1,11 +1,18 @@
 describe "test gcd" do
 	
-	it "one and one" do
-		expect(1).to eq(gcd(1,3));
+	it "reflective" do
+		expect(1).to eq(gcd(1,1))
+		expect(2).to eq(gcd(2,2))
+		expect(-3).to eq(gcd(-3,-3))
 	end
 	
 end
  
 def gcd(a,b)
-return 1
+	while b != 0
+		t = b
+		b = a % t
+		a = t
+	end
+	return a
 end
